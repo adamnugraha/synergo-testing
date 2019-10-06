f37dae1441159d368ce12cbee1669e268b88d6c7
adamnugraha <adamnugraha@yahoo.co.uk>
2019-10-06 19:27:56 +0700

    Automatic build and publish using jenkins

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,7 +23,7 @@ namespace SynergoTesting.Controllers
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
-            ViewBag.NewVariable = "Hello!!!";
+            ViewBag.NewVariable = "Synergo, Hello!!!";
 
             return View();
         }
